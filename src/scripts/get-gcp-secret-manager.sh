4efc0b862a1bd7d33728a340f50a3e3363bc568e
ichi-01 <ichikawa@rhems-japan.co.jp>
2021-10-19 13:37:22 +0900

    return

--- src/scripts/get-gcp-secret-manager.sh
+++ src/scripts/get-gcp-secret-manager.sh
@@ -1,7 +1,7 @@
-LATEST_VER=$(gcloud secrets versions list $SECRET_NAME --limit=1 | awk '{print $1}' | sed -n 2p)
-echo LATEST_VER = $LATEST_VER
+LATEST_VER=$(gcloud secrets versions list ${SECRET_NAME} --limit=1 | awk '{print $1}' | sed -n 2p)
+echo LATEST_VER = ${LATEST_VER}
 
-gcloud secrets versions access $LATEST_VER \
-  --secret=$SECRET_NAME --format='get(payload.data)' |
+gcloud secrets versions access ${LATEST_VER} \
+  --secret=${SECRET_NAME} --format='get(payload.data)' |
   tr '_-' '/+' |
-  base64 -d >>./$OUTPUT_PATH
+  base64 -d >>./${OUTPUT_PATH}
